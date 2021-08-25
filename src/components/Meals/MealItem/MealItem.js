e40a0e65e471e23cd6433819a96052d745bca4a9
Hasan <redfire5005@gmail.com>
2021-08-25 14:37:04 +0300

    Adding Meals and input component with css and fixing form id bug

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import MealItemForm from './MealItemForm';
 import classes from './MealItem.module.css';
 const MealItem = (props) => {
   const price = `$${props.price.toFixed(2)}`;
@@ -10,8 +11,8 @@ const MealItem = (props) => {
         <div className={classes.description}>{props.description}</div>
         <div className={classes.price}>{price}</div>
       </div>
-      <div></div>
-    </li>
+      <div><MealItemForm/></div>
+    </li>   
   );
 };
 
