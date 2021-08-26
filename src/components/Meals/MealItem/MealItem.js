8d9ed8b03ba73ba04eddd48ee8106f006b2e81bd
Hasan <redfire5005@gmail.com>
2021-08-26 14:29:31 +0300

    Making Add to cart work using ref and context

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import MealItemForm from './MealItemForm';
 import classes from './MealItem.module.css';
+import CartContext from '../../../store/cart-context';
+
 const MealItem = (props) => {
+  const cartCtx = useContext(CartContext);
+
   const price = `$${props.price.toFixed(2)}`;
+
+  const addToCartHandler = (amount) => {
+    cartCtx.addItem({
+      id: props.id,
+      name: props.name,
+      amount: amount,
+      price: props.price,
+    });
+  };
 
   return (
     <li className={classes.meal}>
@@ -11,8 +24,10 @@ const MealItem = (props) => {
         <div className={classes.description}>{props.description}</div>
         <div className={classes.price}>{price}</div>
       </div>
-      <div><MealItemForm/></div>
-    </li>   
+      <div>
+        <MealItemForm onAddToCart={addToCartHandler} />
+      </div>
+    </li>
   );
 };
 
