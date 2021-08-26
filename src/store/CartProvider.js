218e67c3514ed0cb1a967268ad4156bf85ffbeb3
Hasan <redfire5005@gmail.com>
2021-08-26 15:24:34 +0300

    Making Cart dynamic with complex Reducer Logic to organize cart items

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -8,15 +8,36 @@ const defaultCartState = {
 
 const cartReducer = (state, action) => {
   if (action.type === 'ADD') {
-    const updatedItems = state.items.concat(action.item);
-    const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount
-  return {
-      items:updatedItems,
-      totalAmount:updatedTotalAmount
+    const updatedTotalAmount =
+      state.totalAmount + action.item.price * action.item.amount;
+
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.item.id
+    );
+
+    const existingCartItem = state.items[existingCartItemIndex];
+    let updatedItems;
+
+    if (existingCartItem) {
+      const updatedItem = {
+        ...existingCartItem,
+        amount: existingCartItem.amount + action.item.amount,
+      };
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    } else {
+      updatedItems = state.items.concat(action.item);
+    }
+
+    //const updatedItems = state.items.concat(action.item);
+
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    };
   }
+  return defaultCartState;
 };
-return defaultCartState;
-}
 
 const CartProvider = (props) => {
   const [cartstate, dispatchCartAction] = useReducer(
