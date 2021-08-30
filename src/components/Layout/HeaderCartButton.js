dce757efd37bf08a6df94d2dd7aedbc2acf142df
Hasan <redfire5005@gmail.com>
2021-08-30 12:05:57 +0300

    Using useEffect for cart header button and finshing the project..

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,21 +1,41 @@
-import React ,{useContext} from 'react'
-import CartIcon from '../Cart/CartIcon'
-import classes from './HeaderCartButton.module.css'
-import CartContext from '../../store/cart-context'
+import React, { useState, useEffect, useContext } from 'react';
+import CartIcon from '../Cart/CartIcon';
+import classes from './HeaderCartButton.module.css';
+import CartContext from '../../store/cart-context';
 const HeaderCartButton = (props) => {
-    const cartCtx = useContext(CartContext)
-    const numberOfCartItems = cartCtx.items.reduce((curNumber,item) => {
-    return curNumber + item.amount  
-    },0)
-    return (
-        <button className={classes.button}  onClick={props.onClick}>
-           <span className={classes.icon}>
-               <CartIcon />
-               </span> 
-           <span>Your Cart</span>
-           <span className={classes.badge}>{numberOfCartItems}</span>
-        </button>
-    )
-}
+  const [btnIsHighLighted, setBtnIsHighLighted] = useState(false);
+  const cartCtx = useContext(CartContext);
+  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
+  }, 0);
 
-export default HeaderCartButton
+  const { items } = cartCtx;
+  const btnClasses = `${classes.button} ${
+    btnIsHighLighted ? classes.bump : ''
+  } `;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnIsHighLighted(true);
+    const timer = setTimeout(() => {
+      setBtnIsHighLighted(false);
+    }, 300);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
+  return (
+    <button className={btnClasses} onClick={props.onClick}>
+      <span className={classes.icon}>
+        <CartIcon />
+      </span>
+      <span>Your Cart</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
+    </button>
+  );
+};
+
+export default HeaderCartButton;
