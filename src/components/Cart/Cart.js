b43d7ac5d0a4d9768629f32d54afea6b11625fca
Hasan <redfire5005@gmail.com>
2021-08-31 14:52:07 +0300

    Fixing -0 small bug for adding more than 20 item in cart and removing it

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -6,7 +6,7 @@ import CartItem from './CartItem';
 const Cart = (props) => {
   const cartCtx = useContext(CartContext);
 
-  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)*1.00}`;
 
   const hasItems = cartCtx.items.length > 0;
 
